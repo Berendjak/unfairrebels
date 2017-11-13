0503e637ae2063b4a48e55e8acc1252f4c0483bd
Berendjak <berend.jak@cobrasystems.nl>
2017-11-13 13:29:32 +0100

    added home and level component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LevelComponent } from './level/level.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LevelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/level/level.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-level',
+  templateUrl: './level.component.html',
+  styleUrls: ['./level.component.sass'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LevelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
