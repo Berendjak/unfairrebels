ee23775cb614b0575e21ed87490e1666db252422
Rick Wiering <rick.wiering5@gmail.com>
2017-11-15 15:16:48 +0100

    Fixed canvas in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,87 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+class Character {
+  public width;
+  public height;
+  public speedX;
+  public speedY;
+  public x;
+  public y;
+  public color;
+
+  constructor(public ctx, width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+  }
+  public update() {
+    this.newPos();
+    this.ctx.fillStyle = this.color;
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  public newPos() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  public moveup() {
+    this.speedY -= 1;
+  }
+
+  public movedown() {
+    this.speedY += 1;
+  }
+
+  public moveleft() {
+    this.speedX -= 1;
+  }
+
+  public moveright() {
+    this.speedX += 1;
+  }
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  public ctx;
+  private character: Character;
+
   @ViewChild('myCanvas')
   public canvasRef: ElementRef;
+  public canvas;
+  private interval;
+
+  public clear = () => {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  ngAfterViewInit(): void {
+    this.canvas = this.canvasRef.nativeElement;
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.ctx = this.canvas.getContext('2d');
+
+    this.startGame();
+  }
+
+  public updateGameArea() {
+    this.clear();
+    this.character.update();
+  }
+
+  public startGame() {
+    this.character = new Character(this.ctx, 30, 30, 'red', 10, 120);
+    this.interval = setInterval(() => this.updateGameArea(), 20);
+  }
+
+
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,24 +2,6 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 import {AppComponent} from '../app.component';
 
-class Ball {
-  constructor(private ctx) {}
-
-  public x = 325;
-  public y = 350;
-  public radius = 25;
-  public color = 'blue';
-
-  public draw() {
-    this.x += 1;
-    this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    this.ctx.closePath();
-    this.ctx.fillStyle = this.color;
-    this.ctx.fill();
-  }
-}
-
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -30,15 +12,8 @@ class Ball {
 export class GameComponent implements OnInit {
   constructor(private canvas: AppComponent) {}
 
-  private ctx;
-
   ngOnInit() {
-    this.ctx = this.canvas.canvasRef.nativeElement.getContext('2d');
-
-    const ball = new Ball(this.ctx);
-    ball.draw();
+    // this.canvas.startGame();
   }
-
   // Functions
-
 }
