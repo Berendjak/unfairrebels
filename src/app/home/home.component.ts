281414198e407af9b9731195a3901e38e100eb34
Niyazi <niyazican60@gmail.com>
2017-11-17 12:44:58 +0100

    added: background music with mute function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {textDef} from "@angular/core/src/view";
+import {createText} from "@angular/core/src/view/text";
 
 @Component({
   selector: 'app-home',
@@ -10,11 +12,29 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   // Variables
+  public audio = new Audio();
+  public buttonStatus = 'MUTE';
 
-  ngOnInit() {
+  public toggle = true;
+
+    ngOnInit() {
     // In here goes what needs to happen at the same time the page loads
+    this.audio.src = '../assets/sounds/theme.mp3';
+    this.audio.load();
+    this.audio.play()
   }
 
-  // Functions
+  // Deze is geluid bij het laden van de pagina
+  public sound() {
+      this.toggle = !this.toggle;
+      this.buttonStatus = this.toggle ? 'MUTE' : 'SOUND'
 
+      if (this.toggle) {
+        this.audio.src = '../assets/sounds/theme.mp3';
+        this.audio.load();
+        this.audio.play();
+      } else {
+        this.audio.src = '';
+      }
+  }
 }
